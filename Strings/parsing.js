4c7f23c8736fe1657b5c867daa87802f20f859fd
Etson <etsond@ufl.edu>
2024-05-01 12:33:53 -0400

    parsing exercise

---
+++ Strings/parsing.js
@@ -0,0 +1,12 @@
+"use strict";
+
+
+function parseAndDisplayName(name) {
+
+    console.log(`Name: ${name} \nFirst: ${name.substring(0, 6)} \nLast: ${name.substring(7)}\n`);
+}
+
+parseAndDisplayName("Brenda Kaye");
+parseAndDisplayName("Ian Auston");
+parseAndDisplayName("Siddalee Grace");
+
