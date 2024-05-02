2178f679aad107dad3df7b73e7707ae984327931
Etson <etsond@ufl.edu>
2024-05-01 21:36:50 -0400

    replacing string using regex

---
+++ Strings/replace_string.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const message = "Our corporate office is located in daLlas";
+
+const findString = message.replace(/dallas/i, "Austin.");
+// console.log(message);
+console.log(findString);
